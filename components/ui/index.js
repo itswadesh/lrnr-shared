e66282404ea6b9adec55f83696200302111bb621
Swadesh Behera <itswadesh@gmail.com>
2020-12-17 23:21:17 +0530

    cipboard implemented

--- components/ui/index.js
+++ components/ui/index.js
@@ -1,5 +1,6 @@
 import Radio from './Radio'
 import Textbox from './Textbox'
 import BackToTopDark from './BackToTopDark'
+import Loading from './Loading'
 
-export { Radio, Textbox,BackToTopDark }
+export { Radio, Textbox,BackToTopDark,Loading }
